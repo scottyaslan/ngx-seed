c25ba3daed494f8278607a73d9707024cb239e71
RynoRn <horvathlaszlo88@gmail.com>
2019-02-27 15:16:11 +0100

    Fixing paths in Gruntfile and adding Browsersync.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,13 @@
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
+ * (the 'License'); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,6 +20,12 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
+        watch: {
+            files: [
+                'src/app/theming/**/*.scss'
+            ],
+            tasks: ['compile-web-ui-styles']
+        },
         sass: {
             options: {
                 outputStyle: 'compressed',
@@ -27,7 +33,7 @@ module.exports = function (grunt) {
             },
             minifyWebUi: {
                 files: [{
-                    './app/css/app-demo.min.css': ['./app/theming/app-demo.scss']
+                    './src/dist/app-demo.min.css': ['./src/app/theming/app-demo.scss']
                 }]
             }
         },
@@ -42,11 +48,11 @@ module.exports = function (grunt) {
             },
             bundleWebUi: {
                 options: {
-                    configFile: "./app/systemjs.builder.config.js"
+                    configFile: './systemjs.config.js'
                 },
                 files: [{
-                    "src": "./app/app-bootstrap.js",
-                    "dest": "./app/app.bundle.min.js"
+                    'src': './src/app/app-bootstrap.js',
+                    'dest': './src/dist/app.bundle.min.js'
                 }]
             }
         },
@@ -57,7 +63,7 @@ module.exports = function (grunt) {
             webUi: {
                 files: [{
                     expand: true,
-                    src: ['./app/app.bundle.min.js'],
+                    src: ['./src/dist/app.bundle.min.js'],
                     dest: './',
                     ext: '.bundle.min.js.gz'
                 }]
@@ -65,13 +71,36 @@ module.exports = function (grunt) {
             webUiStyles: {
                 files: [{
                     expand: true,
-                    src: ['./app/css/app-demo.min.css'],
+                    src: ['./src/dist/app-demo.min.css'],
                     dest: './',
                     ext: '.min.css.gz'
                 }]
+            }
+        },
+        browserSync: {
+            bsFiles: {
+                src : [
+                    // JS files
+                    'src/**/*.js',
+
+                    // CSS files
+                    'src/dist/*.css',
+
+                    // HTML files
+                    'src/**/*.html'
+                ]
+            },
+            options: {
+                port: 8080,
+                watchTask: true,
+                startPath: '/src',
+                server: {
+                    baseDir: './'
+                }
             }
         }
     });
     grunt.registerTask('compile-web-ui-styles', ['sass:minifyWebUi', 'compress:webUiStyles']);
     grunt.registerTask('bundle-web-ui', ['systemjs:bundleWebUi', 'compress:webUi']);
+    grunt.registerTask('default', ['compile-web-ui-styles', 'browserSync', 'watch']);
 };
